5d29c9e2b2635bd4a13df8723c1f1f400b960095
UmarovIkrom <ikromme13@gmail.com>
2022-01-24 01:52:04 +0500

    script.js updated

--- js/script.js
+++ js/script.js
@@ -1,9 +1,23 @@
 "use strict";
 
-    //LOADER
 document.addEventListener("DOMContentLoaded", () => {
-   const
+   const tabs = require('./modules/tabs'),
+         slides = require('./modules/slides'),
+         modal = require('./modules/modal'),
+         loader = require('./modules/loader'),
+         form = require('./modules/form'),
+         data = require('./modules/data'),
+         classes = require('./modules/classes'),
+         accordion = require('./modules/accordion');
 
+    tabs();
+    slides();
+    modal();    
+    loader();    
+    form();
+    data();
+    classes();    
+    accordion();
 }); 
     
     
